ca2d5cd41e75c630213c3d7aa0a76305bac941fe
Aparna Aravind <aparna.pa@v2solutions.com>
2023-01-10 10:16:16 +0530

    Basic Template

---
+++ src/components/todo-list.vue
@@ -0,0 +1,46 @@
+<template>
+    <div id="container">
+      <h2 class="text-center mt-5">LIST YOUR TASKS HERE</h2>
+  
+      <div class="d-flex">
+        <input v-model="task" type="text" placeholder="Enter Task" class="form-control" />
+        <button class="btn btn-warning rounded-0">SUBMIT</button>
+      </div>
+  
+      <!--todo task table -->
+      <table class="table table-bordered mt-5">
+        <thead>
+          <tr>
+            <th scope="col">TASK</th>
+            <th scope="col" style="width: 120px">STATUS</th>
+            <th scope="col" class="text-center">EDIT</th>
+            <th scope="col" class="text-center">DELETE</th>
+          </tr>
+        </thead>
+      </table>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        tasks: [
+          {
+            name: "Learn vue and practice.",
+            status: "to-do"
+          },
+          {
+            name: "Finish Assignments.",
+            status: "In Progress"
+          }
+        ]
+      };
+    },
+    methods: {
+    }
+  };
+  </script>
+  
+  <style scoped>
+  </style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import 'boostrap/dist/css/boostrap.min.css'
 
 createApp(App).mount('#app')
