cc23e729aa279d4f0af8598616e0e7301ba6aa40
Aparna Aravind <aparna.pa@v2solutions.com>
2023-02-20 17:35:59 +0530

    Monster Slayer

---
+++ practice/prj-monster-01-starting-setup/app.js
@@ -0,0 +1,94 @@
+// generic function defined Outside
+function attackCalc(max, min) {
+    return Math.floor(Math.random(max - min)) + min;
+}
+
+const app = Vue.createApp({
+    data() {
+        return {
+            monsterHealth: 100,
+            playerHealth: 100,
+            gameCount: 0,
+            winner: null,
+        }
+    },
+    methods: {
+        attackMonster() {
+            console.log("ATTACKING MONSTER");
+            this.gameCount++;
+            const attackValue = attackCalc(12, 5);
+            this.monsterHealth -= attackValue;
+            this.attackPlayer(); // If monster is attacked,Then it attacks the player back
+        },
+        attackPlayer() {
+            console.log("ATTACKING PLAYER");
+            const attackValue = attackCalc(18, 10);
+            this.playerHealth -= attackValue;
+        },
+        specialAttackMonster() {
+            console.log(" SPECIAL ATTACKING MONSTER");
+            this.gameCount++;
+            const attackValue = attackCalc(25, 10);
+            this.monsterHealth -= attackValue;
+            this.attackPlayer(); // If monster is attacked,Then it attacks the player back
+        },
+        healPlayer() {
+            this.gameCount++;
+            const healValue = attackCalc(8, 20);
+            if (this.playerHealth + healValue > 100) {
+                this.playerHealth = 100;
+            } else {
+                this.playerHealth += healValue;
+            }
+            this.attackPlayer();
+        },
+        surrender() {
+            this.winner = "monster";
+        },
+        startnewGame() {
+            this.monsterHealth = 100;
+            this.playerHealth = 100;
+            this.gameCount = 0;
+            this.winner = null;
+        }
+    },
+    watch: {
+        playerHealth(value) {
+            if (value <= 0 && this.monsterHealth <= 0) {
+                this.winner = 'draw';
+            }
+            else if (value < 0) {
+                this.winner = 'monster';
+            }
+        },
+        monsterHealth(value) {
+            if (value <= 0 && this.playerHealth <= 0) {
+                this.winner = 'draw';
+            }
+            else if (value < 0) {
+                this.winner = 'player';
+            }
+        }
+    },
+
+
+    //Inline style Logic is shifted to computed properties.
+    computed: {
+        monsterHealthStyle() {
+            if (this.monsterHealth < 0) {
+                return { width: '0%' };
+            }
+            return { width: this.monsterHealth + '%' };
+        },
+        playerHealthStyle() {
+            if (this.playerHealth < 0) {
+                return { width: '0%' };
+            }
+            return { width: this.playerHealth + '%' };
+        },
+        specialAttackDisable() { // Can be clicked once in 3 rounds
+            return (this.gameCount % 3 !== 0);
+        }
+    }
+})
+app.mount("#game");
